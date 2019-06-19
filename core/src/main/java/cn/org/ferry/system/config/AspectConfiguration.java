1b77001ec7c0be3ef471eba4c4dc7d23d3923af1
ferry <ferry_sy@163.com>
2019-06-19 14:58:14 +0800

    asprctj demo

---
+++ core/src/main/java/cn/org/ferry/system/config/AspectConfiguration.java
@@ -0,0 +1,18 @@
+package cn.org.ferry.system.config;
+
+import cn.org.ferry.system.aspect.AspectDemo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * Spring AOP 配置类
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class AspectConfiguration {
+    @Bean
+    public AspectDemo aspectDemo() {
+        return new AspectDemo();
+    }
+}
