d1d011c83f8545da9be7e7718cf610e8a2930093
ferry <ferry_sy@163.com>
2019-08-10 11:24:47 +0800

    修改跨域访问配置

--- core/src/main/java/cn/org/ferry/system/config/CORSFilterConfiguration.java
+++ core/src/main/java/cn/org/ferry/system/config/CORSFilterConfiguration.java
@@ -1,55 +1,64 @@
 package cn.org.ferry.system.config;
 
-import org.apache.commons.lang3.StringUtils;
+import cn.org.ferry.system.exception.CommonException;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 /**
  * 注册跨域访问类
  */
 @Configuration
 public class CORSFilterConfiguration {
-    @Value("${ferry.cors.allowedOrigins}")
-    private String allowedOrigins;
+    @Value("#{'${ferry.cors.allowedOrigins}'.split(',')}")
+    private List<String> allowedOrigins;
+    @Value("#{'${ferry.cors.allowedHeaders}'.split(',')}")
+    private List<String> allowedHeaders;
+    @Value("#{'${ferry.cors.allowedMethod}'.split(',')}")
+    private List<String> allowedMethods;
+    @Value("${ferry.cors.maxAge}")
+    private Long maxAge;
+    @Value("${ferry.cors.url}")
+    private String url;
 
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // 设置允许的网站域名，如果全允许则设为 *
-        if(StringUtils.isEmpty(allowedOrigins)){
+        if(CollectionUtils.isEmpty(allowedOrigins)){
             config.addAllowedOrigin("*");
         }else{
-            String[] allow_origins = allowedOrigins.split(",");
-            for(int i = 0; i < allow_origins.length; i++){
-                config.addAllowedOrigin(allow_origins[i]);
+            for (String allowedOrigin : allowedOrigins) {
+                config.addAllowedOrigin(allowedOrigin);
             }
         }
-
         // 设置请求头允许范围
-        config.addAllowedHeader("DNT");
-        config.addAllowedHeader("X-Mx-ReqToken");
-        config.addAllowedHeader("Keep-Alive");
-        config.addAllowedHeader("User-Agent");
-        config.addAllowedHeader("If-Modified-Since");
-        config.addAllowedHeader("Cache-Control");
-        config.addAllowedHeader("Content-Type");
-        config.addAllowedHeader("_token");
+        if(CollectionUtils.isEmpty(allowedHeaders)){
+            throw new CommonException("未设置允许的请求头!");
+        }else{
+            for (String allowedHeader : allowedHeaders) {
+                config.addAllowedHeader(allowedHeader);
+            }
+        }
         // 设置请求方式的允许范围
-        config.addAllowedMethod(HttpMethod.GET);
-        config.addAllowedMethod(HttpMethod.POST);
-        config.addAllowedMethod(HttpMethod.OPTIONS);
-        config.addAllowedMethod(HttpMethod.DELETE);
-        config.addAllowedMethod(HttpMethod.PUT);
+        if(CollectionUtils.isEmpty(allowedMethods)){
+            throw new CommonException("未设置允许的请求方式!");
+        }else{
+            for (String allowedMethod : allowedMethods) {
+                config.addAllowedMethod(allowedMethod);
+            }
+        }
         // 设置预检请求的最大缓存范围
-        config.setMaxAge(1800L);
+        config.setMaxAge(maxAge);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
+        source.registerCorsConfiguration(url, config);
         return new CorsFilter(source);
     }
 }
