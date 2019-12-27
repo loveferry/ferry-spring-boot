4da2b5ecd96b0dfc96138a0c317a0bc766a26846
ferry <ferry_sy@163.com>
2019-12-27 12:44:48 +0800

    数据库模块脚本替换

--- core/src/test/java/DemoTest.java
+++ core/src/test/java/DemoTest.java
@@ -1,7 +1,12 @@
 import cn.org.ferry.system.exception.CommonException;
+import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * <p>description
@@ -24,5 +29,25 @@ public class DemoTest {
         if(StringUtils.equals("b", msg)){
             throw new CommonException("测试");
         }
+    }
+
+
+
+    @Test
+    public void mapRemoveTest(){
+        Map<String, String> map = new HashMap<>(5);
+        map.put("s", "s");
+        map.put("a", "a");
+        map.put("d", "d");
+        map.put("f", "f");
+        map.put("g", "g");
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<String, String> entry = iterator.next();
+            if(StringUtils.equals("s", entry.getKey())){
+                iterator.remove();
+            }
+        }
+        System.out.println(MapUtils.toProperties(map).toString());
     }
 }
